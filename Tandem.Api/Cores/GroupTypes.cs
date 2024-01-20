ffb01a1661b0d7d89e3b14c18cea0b3a7a03deb4
oleksandr <alexkostrytsia@gmail.com>
2024-01-21 01:01:07 +0200

    [core] : add common entities

---
+++ Tandem.Api/Cores/GroupTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace Tandem.Api.Cores;
+
+public class GroupTypes
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+}
---
+++ Tandem.Api/Cores/Student.cs
@@ -0,0 +1,7 @@
+﻿namespace Tandem.Api.Cores;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string? FullName { get; set; }
+}
