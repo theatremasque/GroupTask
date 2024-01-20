66fe1f4c167ddb1d539a42e9ca66255e71230200
oleksandr <alexkostrytsia@gmail.com>
2024-01-21 00:37:49 +0200

    [infrastructure] : add folder, context and connection string

---
+++ Tandem.Api/Infrastructure/GroupDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tandem.Api.Infrastructure;
+
+public class GroupDbContext : DbContext
+{
+    
+    public GroupDbContext(DbContextOptions options) : base(options) { }
+}
