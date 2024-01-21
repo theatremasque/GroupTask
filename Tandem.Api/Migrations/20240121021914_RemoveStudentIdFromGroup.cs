813829182ce0ebeb65a5a66eebb031aa25eaf124
oleksandr <alexkostrytsia@gmail.com>
2024-01-21 04:22:03 +0200

    'db' : migrations

---
+++ Tandem.Api/Migrations/20240121021914_RemoveStudentIdFromGroup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tandem.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveStudentIdFromGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Students_StudentId",
+                table: "Groups");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentId",
+                table: "Groups",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+    }
+}
