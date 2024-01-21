60da3e1ac2c1984776ac92ca25b2556595c166f0
oleksandr <alexkostrytsia@gmail.com>
2024-01-21 02:04:41 +0200

    'core' : add additional entities

---
+++ Tandem.Api/Cores/SubGroup.cs
@@ -0,0 +1,11 @@
+﻿namespace Tandem.Api.Cores;
+
+public class SubGroup
+{
+    public int Id { get; set; }
+    public int GroupId { get; set; }
+    public int StudentId { get; set; }
+
+    public Group Group { get; set; }
+    public Student Student { get; set; }
+}
