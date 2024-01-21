8cdac95816f227216f0c6e1f66e1cdce6b73b9e8
oleksandr <alexkostrytsia@gmail.com>
2024-01-21 02:25:40 +0200

    'infrastructure' : remove DbSet<GroupType>

--- Tandem.Api/Infrastructure/GroupDbContext.cs
+++ Tandem.Api/Infrastructure/GroupDbContext.cs
@@ -7,7 +7,6 @@ public class GroupDbContext : DbContext
 {
     public DbSet<Student> Students { get; set; }
     public DbSet<Group> Groups { get; set; }
-    public DbSet<GroupType> GroupTypes { get; set; }
     public DbSet<SubGroup> SubGroups { get; set; }
     public DbSet<LearnGroup> LearnGroups { get; set; }
     public GroupDbContext(DbContextOptions options) : base(options) { }
