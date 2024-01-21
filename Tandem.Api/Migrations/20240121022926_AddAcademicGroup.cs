fa5caff3a67bf3b4d2535a25e133c94058d570f2
oleksandr <alexkostrytsia@gmail.com>
2024-01-21 04:30:41 +0200

    'db' : add migrations academic group

---
+++ Tandem.Api/Migrations/20240121022926_AddAcademicGroup.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Tandem.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAcademicGroup : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Groups_Students_StudentId",
+                table: "Groups");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_StudentId",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "StudentId",
+                table: "Groups");
+
+            migrationBuilder.CreateTable(
+                name: "AcademicGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GroupId = table.Column<int>(type: "integer", nullable: false),
+                    StudentId = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AcademicGroups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AcademicGroups_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AcademicGroups_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AcademicGroups_GroupId",
+                table: "AcademicGroups",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AcademicGroups_StudentId",
+                table: "AcademicGroups",
+                column: "StudentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AcademicGroups");
+
+            migrationBuilder.AddColumn<int>(
+                name: "StudentId",
+                table: "Groups",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_StudentId",
+                table: "Groups",
+                column: "StudentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Groups_Students_StudentId",
+                table: "Groups",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "Id");
+        }
+    }
+}
