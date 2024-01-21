845ade7e1df7d29e34795bcab8a7b852c7098f5e
oleksandr <alexkostrytsia@gmail.com>
2024-01-21 04:40:10 +0200

    'db' : add fix of migrations

---
+++ Tandem.Api/Migrations/20240121023451_AddAcademicGroupIndex.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tandem.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAcademicGroupIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AcademicGroups_GroupId",
+                table: "AcademicGroups");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AcademicGroups_GroupId_StudentId",
+                table: "AcademicGroups",
+                columns: new[] { "GroupId", "StudentId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AcademicGroups_GroupId_StudentId",
+                table: "AcademicGroups");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AcademicGroups_GroupId",
+                table: "AcademicGroups",
+                column: "GroupId");
+        }
+    }
+}
