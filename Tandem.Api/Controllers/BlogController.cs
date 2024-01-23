b5b90bd6e7b7333ec9ba4d25a223f70632bbbe2f
theatremasque <alexkostrytsia@gmail.com>
2024-01-23 13:14:42 +0200

    'app' : add action for blog

---
+++ Tandem.Api/Controllers/BlogController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tandem.Api.Infrastructure;
+
+namespace Tandem.Api.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class BlogController : ControllerBase
+{
+    private readonly TandemDbContext _ctx;
+
+    public BlogController(TandemDbContext ctx)
+    {
+        _ctx = ctx;
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult> BlogFunction()
+    {
+        var q = await _ctx.Blogs.Where(b => _ctx.ActivePostCountForBlog(b.BlogId) > 0)
+            .Select(s => s)
+            .ToListAsync();
+        
+        return Ok(q);
+    } 
+}
